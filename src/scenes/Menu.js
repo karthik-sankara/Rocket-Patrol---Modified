775bd0543fd7db5de49e11937faa8c40aa355b51
“Karthik” <kssankar@ucsc.edu>
2023-10-20 22:26:20 -0700

    additions to title screen

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -15,11 +15,13 @@ class Menu extends Phaser.Scene {
 
     create() {
         // menu text configuration
+      this.cameras.main.setBackgroundColor('#0000FF'); // Set to blue color
+
       let menuConfig = {
-          fontFamily: 'Courier',
+          fontFamily: 'Impact',
           fontSize: '28px',
-          backgroundColor: '#F3B141',
-          color: '#843605',
+          backgroundColor: 'orange',
+          color: 'black',
           align: 'right',
           padding: {
               top: 5,
@@ -28,29 +30,29 @@ class Menu extends Phaser.Scene {
           fixedWidth: 0
       }
         let highScoreConfig = {
-          fontFamily: 'Courier',
+          fontFamily: 'Meslo',
           fontSize: '28px',
           backgroundColor: '#F3B141',
           color: '#843605',
-          align: 'right',
+          align: 'left',
           padding: {
               top: 5,
               bottom: 5,
           },
-          fixedWidth: 250
+          fixedWidth: 200
       }
         
         // show menu text
-        this.add.text(game.config.width/2, game.config.height/2 - borderUISize - borderPadding, 'ROCKET PATROL', menuConfig).setOrigin(0.5);
+        this.add.text(game.config.width/2, 120, 'ROCKET PATROL', menuConfig).setOrigin(0.5);
         this.add.text(game.config.width/2, game.config.height/2, 'Use ←→ arrows to move & (F) to fire', menuConfig).setOrigin(0.5);
-        menuConfig.backgroundColor = '#00FF00';
-        menuConfig.color = '#000';
-        this.add.text(game.config.width/2, game.config.height/2 + borderUISize + borderPadding, 'Press ← for Novice or → for Expert', menuConfig).setOrigin(0.5);
-        
+        menuConfig.backgroundColor = 'red';
+        menuConfig.color = 'pink';
+        this.add.text(game.config.width/2, game.config.height/2 + borderUISize + borderPadding, 'Press ← for Beginners or → for Experienced', menuConfig).setOrigin(0.5);
         
 
-        this.highScoreLeft = this.add.text(borderUISize + borderPadding, borderUISize + borderPadding*2, highScoreTracker, highScoreConfig);
+        this.highScoreLeft = this.add.text((game.config.width/2) - 100, 400, highScoreTracker, highScoreConfig);
         this.highScoreLeft.text = 'High Score: ' + highScoreTracker;
+
 
 
         // define keys
